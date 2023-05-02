e2ce8801bcf928f2abdb834bfde75f799da5f0c4
AnyaDroz <anya.drozdova89@gmail.com>
2023-05-02 08:58:14 +0100

    adding-design-system

--- build.js
+++ build.js
@@ -3,127 +3,16 @@ const StyleDictionary = require('style-dictionary');
 registerTransforms(StyleDictionary);
 
 
-// StyleDictionary.registerTransform({
-// 	type: 'value',
-// 	transitive: true,
-// 	name: 'test',
-//   format: "JSON/flat",
-// 	matcher: ({ type }) => {
-// 		return ['typography', 'composition'].includes(type);
-// 	},
-// 	transformer: ({ value, name, type}) => {
-
-// return JSON.stringify(value.Object);
-
-    
-// 	},
-
-// });
-
-
-
-
-// const sd = StyleDictionary.extend({
-//   source: ['**/tokens/CORE.json'],
-//   platforms: {
-//     // js: {
-//     //   transformGroup: 'tokens-studio',
-//     //   buildPath: 'build/js/',
-//     //   files: [
-//     //     {
-//     //       destination: 'CORE.js',
-//     //       format: 'javascript/es6',
-//     //     },
-//     //   ],
-//     // },
-//     css: {
-//       transforms: [
-//         'ts/descriptionToComment',
-//         'ts/size/px',
-//         'ts/opacity',
-//         'ts/size/lineheight',
-//         'ts/type/fontWeight',
-//         'ts/resolveMath',
-//         'ts/size/css/letterspacing',
-//         'ts/typography/css/shorthand',
-//         'ts/border/css/shorthand',
-//         'ts/shadow/css/shorthand',
-//         'ts/color/css/hexrgba',
-//         'ts/color/modifiers',
-//         'name/cti/kebab',
-//       ],
-//       buildPath: 'build/css/',
-//       files: [
-//         {
-//           destination: 'CORE.css',
-//           format: 'css/variables',
-//         },
-//       ],
-//     },
-//   },
-  
-// });
-
-
-
-// sd.cleanAllPlatforms();
-// sd.buildAllPlatforms();
-
-// const sdLight = StyleDictionary.extend({
-//   source: ['**/tokens/LIGHT.json'],
-//   platforms: {
-//     // js: {
-//     //   transformGroup: 'tokens-studio',
-//     //   buildPath: 'build/js/',
-//     //   files: [
-//     //     {
-//     //       destination: 'LIGHT.js',
-//     //       format: 'javascript/es6',
-//     //     },
-//     //   ],
-//     // },
-//     css: {
-//       transforms: [
-//         'ts/descriptionToComment',
-//         'ts/size/px',
-//         'ts/opacity',
-//         'ts/size/lineheight',
-//         'ts/type/fontWeight',
-//         'ts/resolveMath',
-//         'ts/size/css/letterspacing',
-//         'ts/typography/css/shorthand',
-//         'ts/border/css/shorthand',
-//         'ts/shadow/css/shorthand',
-//         'ts/color/css/hexrgba',
-//         'ts/color/modifiers',
-//         'name/cti/kebab',
-//       ],
-//       buildPath: 'build/css/',
-//       files: [
-//         {
-//           destination: 'LIGHT.css',
-//           format: 'css/variables',
-//         },
-//       ],
-//     },
-//   },
-  
-// });
-
-
-
-// sdLight.cleanAllPlatforms();
-// sdLight.buildAllPlatforms();
-
-const sdDark = StyleDictionary.extend({
-  source: ['**/tokens/DARK.json'],
+const sd = StyleDictionary.extend({
+  source: ['**/tokens/CORE.json'],
   platforms: {
+    //if you need js variables
     // js: {
     //   transformGroup: 'tokens-studio',
     //   buildPath: 'build/js/',
     //   files: [
     //     {
-    //       destination: 'DARK.js',
+    //       destination: 'CORE.js',
     //       format: 'javascript/es6',
     //     },
     //   ],
@@ -143,12 +32,82 @@ const sdDark = StyleDictionary.extend({
         'ts/color/css/hexrgba',
         'ts/color/modifiers',
         'name/cti/kebab',
-        // 'test',
-        
-        
+      ],
+      buildPath: 'src/build/css/',
+      files: [
+        {
+          destination: 'CORE.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+  
+});
+
+
+
+sd.cleanAllPlatforms();
+sd.buildAllPlatforms();
+
+const sdLight = StyleDictionary.extend({
+  source: ['**/tokens/LIGHT.json'],
+  platforms: {
+    css: {
+      transforms: [
+        'ts/descriptionToComment',
+        'ts/size/px',
+        'ts/opacity',
+        'ts/size/lineheight',
+        'ts/type/fontWeight',
+        'ts/resolveMath',
+        'ts/size/css/letterspacing',
+        'ts/typography/css/shorthand',
+        'ts/border/css/shorthand',
+        'ts/shadow/css/shorthand',
+        'ts/color/css/hexrgba',
+        'ts/color/modifiers',
+        'name/cti/kebab',
+      ],
+      buildPath: 'src/build/css/',
+      files: [
+        {
+          destination: 'LIGHT.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+  
+});
+
+
+
+sdLight.cleanAllPlatforms();
+sdLight.buildAllPlatforms();
+
+const sdDark = StyleDictionary.extend({
+  source: ['**/tokens/DARK.json'],
+  platforms: {
+    css: {
+      transforms: [
+        'ts/descriptionToComment',
+        'ts/size/px',
+        'ts/opacity',
+        'ts/size/lineheight',
+        'ts/type/fontWeight',
+        'ts/resolveMath',
+        'ts/size/css/letterspacing',
+        'ts/typography/css/shorthand',
+        'ts/border/css/shorthand',
+        'ts/shadow/css/shorthand',
+        'ts/color/css/hexrgba',
+        'ts/color/modifiers',
+        'name/cti/kebab',
+  
         
       ],
-      buildPath: 'build/css/',
+      buildPath: 'src/build/css/',
       files: [
         {
           destination: 'DARK.css',
---
+++ src/designSystem/DesignSystem.styles.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import ReactDOM from "react-dom";
+import "../build/css/LIGHT.css"
+import DesignSystem from "./DesignSystem";
+
+
+export const StyledHeaderContainer = styled.div`
+    color: var(--primary);
+    font-size: 50px;
+    font-weight: bold;
+`;
+
+export const StyledH1 = styled.div`
+
+`;
+export const StyledH2 = styled.div`
+
+`;
+
+
