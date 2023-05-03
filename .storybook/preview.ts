4a36b456eb9ab405d53d6c86375c4fc7468f9be5
AnyaDroz <anya.drozdova89@gmail.com>
2023-05-03 09:23:46 +0100

    trash-branch

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,19 @@
 import type { Preview } from "@storybook/react";
 import "../src/index.css";
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "storybook-addon-themes/react";
+
+addDecorator(withThemes);
 
 const preview: Preview = {
 	parameters: {
+		themes: {
+			default: "light",
+			list: [
+				{ name: "light", class: "theme-twt", color: "#00aced" },
+				{ name: "dark", class: "theme-fb", color: "#3b5998" },
+			],
+		},
 		actions: { argTypesRegex: "^on[A-Z].*" },
 		controls: {
 			matchers: {
