2f32cff81d010fee663a9e480884e964bba30f6d
AnyaDroz <anya.drozdova89@gmail.com>
2023-05-04 07:40:00 +0100

    trying-theme-provider

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,24 +1,60 @@
 import type { Preview } from "@storybook/react";
 import "../src/index.css";
+import ThemeSwitch from "../src/ThemeSwitch/ThemeSwitch";
+
+import { ThemeProvider } from "styled-components";
+
+import { MyThemes } from "../my-theme-folder/my-theme-file";
+
+// Function to obtain the intended theme
+const getTheme = (themeName) => {
+	return MyThemes[themeName];
+};
+
+const withThemeProvider = (Story, context) => {
+	const theme = getTheme(context.globals.theme);
+	return (
+		<ThemeProvider theme={theme}>
+			<Story />
+		</ThemeProvider>
+	);
+};
 
 const preview: Preview = {
-	parameters: {
-		actions: { argTypesRegex: "^on[A-Z].*" },
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/,
+	globalTypes: {
+		theme: {
+			description: "Global theme for components",
+			defaultValue: "light",
+			toolbar: {
+				title: "Theme",
+				icon: "circlehollow",
+
+				items: ["light", "dark"],
+
+				dynamicTitle: true,
 			},
 		},
-		themes: {
-			default: "light",
-			list: [
-				{ name: "light", theme: ["light"], color: "#00aced" },
-				{ name: "dark", theme: ["dark"], color: "#00aced" },
-			],
-			onChange() {},
-		},
+		decorators: [withThemeProvider],
 	},
+	// parameters: {
+	// 	actions: { argTypesRegex: "^on[A-Z].*" },
+	// 	controls: {
+	// 		matchers: {
+	// 			color: /(background|color)$/i,
+	// 			date: /Date$/,
+	// 		},
+	// 	},
+	// 	themes: {
+	// 		default: "light",
+	// 		list: [
+	// 			{ name: "light", color: "#00aced" },
+	// 			{ name: "dark", color: "#00aced" },
+	// 		],
+	// 		onChange(theme) {
+	// 			theme = "dark";
+	// 		},
+	// 	},
+	// },
 };
 
 export default preview;
--- src/designSystem/DesignSystem.stories.ts
+++ src/designSystem/DesignSystem.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { string } from "prop-types";
-import { options } from "style-dictionary";
 
 import DesignSystem from "./DesignSystem";
 
