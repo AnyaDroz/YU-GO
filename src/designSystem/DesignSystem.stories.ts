3a54f58613b368d62edb095bf90e91455c53e0de
AnyaDroz <anya.drozdova89@gmail.com>
2023-05-04 12:13:15 +0100

    trying-new-version

---
+++ src/designSystem/DesignSystem.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import DesignSystem from "./DesignSystem";
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta = {
+	title: "Example/Button",
+	component: DesignSystem,
+	tags: ["autodocs"],
+	argTypes: {
+		backgroundColor: { control: "color" },
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+	args: {
+		primary: true,
+		label: "Button",
+	},
+};
--- src/designSystem/index.tsx
+++ src/designSystem/index.tsx
@@ -1,1 +1,1 @@
-export * from "./DesignSystem"
+export * from "./DesignSystem";
