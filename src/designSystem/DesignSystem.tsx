fcaf0d4f3f627dec95fb4282ba8e3a84bd892966
AnyaDroz <anya.drozdova89@gmail.com>
2023-05-04 11:18:43 +0100

    back-to-start

--- src/designSystem/DesignSystem.tsx
+++ src/designSystem/DesignSystem.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import {StyledH1,
-StyledH2,
-StyledHeaderContainer} from "./DesignSystem.styles"
-const DesignSystem = () => {
-  return (
-    <div>
-        
-        <StyledHeaderContainer>
-            <StyledH1>hello</StyledH1>
-            <StyledH2></StyledH2>
-        </StyledHeaderContainer>
-       
-       
-    </div>
-  )
-}
+import React from "react";
+import * as LightTheme from "../js/LIGHT";
+import { ThemeProvider } from "styled-components";
 
-export default DesignSystem
+import {
+	StyledH1,
+	StyledH2,
+	StyledLabel,
+	StyledSmallLabel,
+	StyledInterfaceDark,
+	StyledParagraph,
+	StyledPrimaryColor,
+	StyledSecondaryColor,
+	StyledLayerBackground,
+	StyledInterfaceLight,
+	StyledBackground,
+	StyledContainer,
+} from "./DesignSystem.styles";
+
+const DesignSystem = () => {
+	return (
+		<div>
+			<ThemeProvider theme={LightTheme}>
+				<StyledContainer>
+					<StyledH1>H1</StyledH1>
+					<StyledH2>H2</StyledH2>
+					<StyledLabel>LABEL</StyledLabel>
+					<StyledSmallLabel>SMALL LABEL</StyledSmallLabel>
+					<StyledParagraph>Paragraph</StyledParagraph>
+					<StyledPrimaryColor />
+					<StyledSecondaryColor />
+					<StyledInterfaceDark />
+					<StyledInterfaceLight />
+					<StyledLayerBackground />
+					<StyledBackground />
+				</StyledContainer>
+			</ThemeProvider>
+		</div>
+	);
+};
+
+export default DesignSystem;
---
+++ src/js/LIGHT.js
@@ -0,0 +1,45 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 04 May 2023 10:16:57 GMT
+ */
+
+export const s13 = "13px";
+export const s16 = "16px";
+export const s20 = "20px";
+export const s25 = "25px";
+export const s31 = "31px";
+export const s38 = "38px";
+export const s48 = "48px";
+export const s60 = "60px";
+export const blue5 = "#E7EAEE";
+export const blue100 = "#D2D1E1";
+export const blue200 = "#B3B3CE";
+export const blue300 = "#8D8CB5";
+export const blue400 = "#68669C";
+export const blue500 = "#424084";
+export const blue600 = "#1C1A6B";
+export const blue700 = "#171659";
+export const blue800 = "#131147";
+export const blue900 = "#131147";
+export const blue1000 = "#090924";
+export const blue1100 = "#060515";
+export const sky50 = "#E6F5FC";
+export const sky100 = "#D6EEFB";
+export const sky200 = "#C1E5F8";
+export const sky300 = "#ADDCF6";
+export const sky400 = "#98D4F4";
+export const sky500 = "#84CBF2";
+export const sky600 = "#6EA9CA";
+export const sky700 = "#5887A1";
+export const sky800 = "#426679";
+export const sky900 = "#2C4451";
+export const sky1000 = "#1A2930";
+export const sky1100 = "#161D21";
+export const unicaOne = "Unica One";
+export const openSans = "Open Sans";
+export const s150 = "1.5";
+export const s130 = "1.3";
+export const s100 = "1";
+export const dropShadow = "inset 0 4px 4px 0 #00000040";
+export const grey = "#F2F5F5";
+export const dark = "#0A0A0A";
